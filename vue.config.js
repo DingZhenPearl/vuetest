bf8661a63c651495a65c026ec99cf8442a49bdc0
deeperthought <deeperthought@noreply.gitcode.com>
2025-05-11 18:42:56 +0800

    build(babel): 添加 Babel 插件以支持私有方法和属性
    - 添加 @babel/plugin-transform-private-methods 插件
    - 添加 @babel/plugin-transform-private-property-in-object 插件
    - 添加 @babel/plugin-transform-class-properties 插件
    - 添加 @babel/plugin-proposal-class-properties 插件

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,26 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  chainWebpack: config => {
+    config.module
+      .rule('js')
+      .include
+      .add(/node_modules\/@tdesign-vue-next\/chat/)
+      .add(/node_modules\/marked/)
+      .end()
+      .use('babel-loader')
+      .loader('babel-loader')
+      .tap(options => {
+        return {
+          ...options,
+          plugins: [
+            ...(options.plugins || []),
+            '@babel/plugin-transform-private-methods',
+            '@babel/plugin-transform-private-property-in-object',
+            '@babel/plugin-transform-class-properties',
+            '@babel/plugin-proposal-class-properties'
+          ]
+        }
+      });
+  }
 })
